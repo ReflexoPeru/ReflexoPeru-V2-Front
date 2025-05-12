e3567c7fd91e8afee88d38ebc99142beacde18b1
JeremyVillodas <luisjeremy_19@hotmail.com>
2025-05-12 11:05:16 -0500

    refactor: :art: Add Router

--- src/components/Header/CustomLayout.jsx
+++ src/components/Header/CustomLayout.jsx
@@ -6,13 +6,9 @@ const { Sider, Content } = Layout;
 
 const CustomLayout = () => {
   return (
-    <Layout style={{ height: '100vh' }}>
-      <Sider width={200} style={{ background: '#fff' }} />
-      <Layout>
-        <CustomHeader title="Crear Paciente" />
-        <Content style={{ padding: '24px', minHeight: 280 }}>{}</Content>
-      </Layout>
-    </Layout>
+    <>
+      <CustomHeader title="Crear Paciente" />
+    </>
   );
 };
 
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,15 +2,56 @@ import { createBrowserRouter, RouterProvider } from 'react-router';
 import Dashboard from '../pages/Dashboard/Dashboard';
 import Login from '../features/auth/ui/login';
 import View from '../pages/View';
+import Prueba from '../pages/prueba';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Login />,
+  },
+  {
+    path: '/Inicio',
+    element: <View />,
     children: [
       {
-        path: '/Inicio',
-        element: <View />,
+        path: 'pacientes',
+        element: <Prueba />,
+      },
+      {
+        path: 'citas',
+        element: <Prueba />,
+      },
+      {
+        path: 'reportes',
+        element: <Prueba />,
+      },
+      {
+        path: 'citasCompletas',
+        element: <Prueba />,
+      },
+      {
+        path: 'estadisticas',
+        element: <Prueba />,
+      },
+      {
+        path: 'terapeutas',
+        element: <Prueba />,
+      },
+      {
+        path: 'configSistema',
+        element: <Prueba />,
+      },
+      {
+        path: 'configPagos',
+        element: <Prueba />,
+      },
+      {
+        path: 'configUser',
+        element: <Prueba />,
+      },
+      {
+        path: 'configPerfil',
+        element: <Prueba />,
       },
     ],
   },
