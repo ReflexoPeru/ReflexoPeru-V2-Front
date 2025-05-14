242c0f37c1aa296ac9d54ab43bac847da212046b
Mirko Limas <mirkodanielramoslimas20@gmail.com>
2025-05-14 12:33:44 -0500

    feat: :art: Create custom 404 error

--- src/pages/Error/Error404.jsx
+++ src/pages/Error/Error404.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from 'antd';
 import styles from './Error404.module.css';
-import imgerror from '../../assets/Img/imgError.png';
+import imgerror from '../../assets/Img/imgerror.png';
 
 const Error = () => {
   return (
@@ -11,16 +11,16 @@ const Error = () => {
           src={imgerror}
           alt="Error"
           className={styles.errorImage}
-          style={{ width: '320px', maxWidth: '100%' }}
+          style={{ width: '320px', maxWidth: '90%' }}
         />
-        <h1 className={styles.title}>Oops!</h1>
+        <h1 className={styles.title}>404</h1>
         <p className={styles.subtitle}>
-          Algo esta fallando.
+          No pudimos encontrar la página que buscas.
           <br />
-          Por favor, intenta de nuevo o contacta al soporte.
+          Quizá fue eliminada, movida o nunca existió.
         </p>
         <Button className={styles.homeButton} size="large">
-          Intentar de nuevo
+          Volver al inicio
         </Button>
       </div>
     </div>
