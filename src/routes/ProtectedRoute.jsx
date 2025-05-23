cf57d481c2a0e025264877cfb539339d14a731ad
JeremyVillodas <luisjeremy_19@hotmail.com>
2025-05-23 13:34:49 -0500

    fix: :bug: Fix Loader

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,16 +1,22 @@
 // src/components/ProtectedRoute.jsx
 import { Navigate, Outlet } from 'react-router';
 import { useAuth } from './AuthContext';
+import Style from './ProtectedRoute.module.css';
 
 const ProtectedRoute = ({ allowedRoles }) => {
   const { isAuthenticated, authChecked, userRole } = useAuth();
 
-  if (!authChecked) return <div>Cargando...</div>;
+  if (!authChecked)
+    return (
+      <div className={Style.container}>
+        <div className={Style.loader}></div>
+      </div>
+    );
 
   if (!isAuthenticated) return <Navigate to="/" replace />;
 
   if (allowedRoles && !allowedRoles.includes(userRole)) {
-    return <Navigate to="/Inicio" replace />; 
+    return <Navigate to="/Inicio" replace />;
   }
 
   return <Outlet />;
