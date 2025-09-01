080f048efc34b194877bcb0715678090899a74d0
RenzoSoftware <renzovillodas47@gmail.com>
2025-09-01 11:38:22 -0500

    fix: corregir etiqueta en el formulario de registro de paciente

--- src/features/patients/ui/RegisterPatient/NewPatient.jsx
+++ src/features/patients/ui/RegisterPatient/NewPatient.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router';
 const fields = [
   {
     type: 'title',
-    label: 'REGISTRAR PACIENTE',
+    label: 'REGISTRARqweqrqwrqwrq',
   },
   {
     type: 'subtitle',
